39f21822d2ba57a2709c07fe2b6c2b39adc0106d
David Johnston <dwtj@iastate.edu>
2019-01-19 13:56:22 -0600

    Reorder function definitions in `agent.cc`
    These definitions now follow the order of their corresponding phases:
    Because PHASE_ONLOAD < PHASE_LIVE < PHASE_DEAD, we list these three
    callbacks OnLoad < OnAttach < OnUnload.

--- phasicj/tracelogger/agent.cc
+++ phasicj/tracelogger/agent.cc
@@ -18,15 +18,15 @@
 
 namespace phasicj::tracelogger {
 
-jint Agent::OnAttach(JavaVM *vm, char *options, void *reserved) {
-  // TODO(dwtj)
-  std::cout << "OnAttach(...)" << std::endl;
-  return JNI_OK;
-}
-
 jint Agent::OnLoad(JavaVM *vm, char *options, void *reserved) {
   // TODO(dwtj)
   std::cout << "OnLoad(...)" << std::endl;
+  return JNI_OK;
+}
+
+jint Agent::OnAttach(JavaVM *vm, char *options, void *reserved) {
+  // TODO(dwtj)
+  std::cout << "OnAttach(...)" << std::endl;
   return JNI_OK;
 }
 
