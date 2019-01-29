c6a83659d03ccb2116076401f4a5bef7ec370692
David Johnston <dwtj@iastate.edu>
2019-01-29 10:15:42 -0600

    WIP: fixed `GetThreadId()` error (boolean flipped)

--- phasicj/util/jvmti/threads.cc
+++ phasicj/util/jvmti/threads.cc
@@ -1,5 +1,7 @@
+#include <string>
 #include <optional>
 
+#include "boost/log/trivial.hpp"
 #include "jni.h"
 #include "jvmti.h"
 
@@ -9,27 +11,35 @@ namespace phasicj::util::jvmti {
 
 std::optional<jlong> GetThreadId(JNIEnv& jni_env, const jthread thread) {
   if (thread == nullptr) {
-    // TODO: Error log this.
+    BOOST_LOG_TRIVIAL(warning) << "GetThreadId() passed a null thread.";
     return {};
   }
+
+  // Wraps checking whether `jni_env` has a pending exception. If so, it is
+  // logged, cleared, and `true` is returned; otherwise, `false` is returned.
+  auto ExceptionCheck = [&jni_env](auto debug_context_message) -> bool {
+    if (jni_env.ExceptionCheck() == JNI_TRUE) {
+      BOOST_LOG_TRIVIAL(debug) << debug_context_message;
+      // TODO(dwtj): Extract this throwable's msg and log it with boost.
+      jni_env.ExceptionDescribe();
+      jni_env.ExceptionClear();
+      return true;
+    }
+
+  };
+
   jclass thread_klass = jni_env.GetObjectClass(thread);
-  if (jni_env.ExceptionCheck() == JNI_FALSE) {
-    // TODO: Error log this.
-    jni_env.ExceptionClear();
+  if (ExceptionCheck("`GetThreadId()` called GetObjectClass().")) {
     return {};
   }
 
   jmethodID method_id = jni_env.GetMethodID(thread_klass, "getId", "()J");
-  if (jni_env.ExceptionCheck() == JNI_FALSE) {
-    // TODO: Error log this.
-    jni_env.ExceptionClear();
-    return {};
+  if (ExceptionCheck("`GetThreadId()` called `GetMethodID()`.")) {
+    return true;
   }
 
   jlong thread_id = jni_env.CallLongMethod(thread, method_id);
-  if (jni_env.ExceptionCheck() == JNI_FALSE) {
-    // TODO: Error log this.
-    jni_env.ExceptionClear();
+  if (ExceptionCheck("`GetThreadId()` called `CallLongMethod()`")) {
     return {};
   }
 
